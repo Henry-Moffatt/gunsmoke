fe7e3feea9be192a24bf9a8c95eb419433725cae
HenryM <hmoffatt.2025@trinity.nsw.edu.au>
2024-06-18 12:21:37 +1000

    Basic classes + simple pygame script

--- main.py
+++ main.py
@@ -1,23 +1,93 @@
-class Destructible():
-    pass
+import pygame
+import random
+from pygame.locals import *
+
+class Destructible:
+    def __init__(self,health,dropChances,dropItems):
+        self.h= health
+        self.dC=dropChances
+        self.dI=dropItems
+    
+    def takeDamage(self, amount):
+        self.h -= amount
+
+    def drop(self):
+        pass
 
 class Box(Destructible):
-    pass
+    def __init__(self, health, dropChances, dropItems):
+        super().__init__(health, dropChances, dropItems)
 
 class Character(Destructible):
-    pass
+    def __init__(self, health, dropChances, dropItems, speed, dmg, fireRate, weapon):
+        super().__init__(health, dropChances, dropItems)
+        self.s = speed
+        self.d = dmg
+        self.f= fireRate
+        self.w = weapon
+
+    def shoot(self, angle):
+        pass
+
+    def move(self, direction):
+        pass
 
 class Player(Character):
-    pass
+    def __init__(self, health, dropChances, dropItems, speed, dmg, fireRate, weapon, score, controls):
+        super().__init__(health, dropChances, dropItems, speed, dmg, fireRate, weapon)
+        self.sc=score
+        self.cont =controls
+    
+    def increaseScore(self, amount):
+        self.sc += amount
+
+    def increaseHealth(self, amount):
+        self.h += amount
+    
+    def increaseSpeed(self, amount):
+        self.s+=amount
+    
+    def increaseFireRate(self, amount):
+        self.f += amount
 
 class Enemy(Character):
-    pass
+    def __init__(self, health, dropChances, dropItems, speed, dmg, fireRate, weapon):
+        super().__init__(health, dropChances, dropItems, speed, dmg, fireRate, weapon)
+
+    def trackPlayer():
+        pass
 
 class BuildingEnemy(Enemy):
-    pass
+    def __init__(self, health, dropChances, dropItems, speed, dmg, fireRate, weapon):
+        super().__init__(health, dropChances, dropItems, speed, dmg, fireRate, weapon)
 
 class Projectile():
-    pass
+    def __init__(self, dmg, angle, speed):
+        self.d = dmg
+        self.a = angle
+        self.s = speed
 
 class Environment():
-    pass
+    def __init__(self, scrollSpeed):
+        self.scr = scrollSpeed
+
+    def scroll(self):
+        pass
+
+    def instantiateBox():
+        pass
+
+    def instantiateBuilding():
+        pass
+
+pygame.init()
+window = pygame.display.set_mode((400,550))
+window.fill((255,255,255))
+running = True
+while running:
+
+    pygame.draw.rect(window, (0, 0, 255), [100, 100, 20, 20], 0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
